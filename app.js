a70570dd16a74b2d6befd5d8972c13522a075866
Martin Salomon <salomonmartin21@gmail.com>
2024-08-29 20:32:31 -0300

    minimo manejo del dom

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log('hola rey');
+let h5 = document.querySelector('.card')
+
+console.log(h5);
+
+
+h5.innerHTML =
+ `
+<div class="card" style="width: 18rem; display: flex;">
+            <img src="./img/el-mensaje-motivador-de-dibu-martinez-en-la-seleccion-argentina-antes-de-la-semifinal-de-la-copa-americb_267x150.webp" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">dibu</h5>
+              <p class="card-text">comelo dibu</p>
+              <a href="#" class="btn btn-primary">comprar una mega dibu</a>
+            </div>
+          </div>
+
+`
