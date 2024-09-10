d9ff34b10d91717d4195c3c7a7cbe0c46bd3ec27
Martin Salomon <salomonmartin21@gmail.com>
2024-09-10 20:14:13 -0300

    usando for

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
-let h5 = document.querySelector('.card')
+let container = document.querySelector('.container')
+let h1 = document.querySelector('h1')
 
-console.log(h5);
+h1.innerText = 'productos'
 
+const cards= []
 
-h5.innerHTML =
- `
-<div class="card" style="width: 18rem; display: flex;">
-            <img src="./img/el-mensaje-motivador-de-dibu-martinez-en-la-seleccion-argentina-antes-de-la-semifinal-de-la-copa-americb_267x150.webp" class="card-img-top" alt="...">
+for(let i=1; i < 7; i++){
+  const card=  `
+        <div class="card" style="width: 18rem; display: flex;">
+            <img src="https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/${i}.jpg" alt="...">
             <div class="card-body">
               <h5 class="card-title">dibu</h5>
               <p class="card-text">comelo dibu</p>
               <a href="#" class="btn btn-primary">comprar una mega dibu</a>
             </div>
-          </div>
-
+         </div>
 `
+  cards.push(card)
+}
+container.innerHTML = cards.join().replaceAll(',', '')
